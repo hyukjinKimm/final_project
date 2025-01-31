39e3204d91ecc08a7bae8c733d9db020315d864d
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-01-31 21:19:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ session_start();
     <!-- loading bar -->
     <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
     <link rel="stylesheet" href="./css/flash.css">
-    <title>Hotel blue bird</title>
+    <title>메타넷 병원</title>
 </head>
 
 <body>
